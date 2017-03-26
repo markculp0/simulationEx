2175e157db196eea4a887f4e41e3012a304a1237
Mark Culp <markculp0@gmail.com>
2017-03-25 22:32:25 -0500

    1st commit

---
+++ sumulationEx.R
@@ -0,0 +1,27 @@
+# Simulation Exercise
+# Statistical Inference
+# Mark Culp
+# 3/25/2017
+
+
+## Overview:
+
+# Set parameters for distribution
+n <- 40
+lambda <- 0.2
+
+
+# Generate random deviates for distribution
+rexp(n, lambda)
+
+
+## Simulations:  
+
+## Sample Mean versus Theoretical Mean
+
+## Sample Variance versus Theoretical Variance
+
+## Distribution
+
+
+
