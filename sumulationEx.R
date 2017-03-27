e3cc9769bb1024cfd885ccb643079d0d177fcc85
Mark Culp <markculp0@gmail.com>
2017-03-26 21:28:49 -0500

    mean & var calc

--- sumulationEx.R
+++ sumulationEx.R
@@ -3,29 +3,56 @@
 # Mark Culp
 # 3/25/2017
 
+
+## --------
 ## Overview:
+## --------
 
 # Set parameters for distribution
 n <- 40
 lambda <- 0.2
 
-# Generate random deviates for distribution
-rexp(n, lambda)
+set.seed(0)
+
+# Generate random deviates for an exponetial distribution
+sampleDist <- rexp(n, lambda)
 
 # An exploratory plot 
 # . . .
 
+## -----------
 ## Simulations:  
+## -----------
 
+
+
+## -----------------------------------
 ## Sample Mean versus Theoretical Mean
+## -----------------------------------
 
+# Calculate mean of the sample distribution
+sampleDistMean <- mean(sampleDist)
+
+# Calculate hypothetical mean for the exponential distribution
+hypoDistMean <- 1/lambda
+
+## -------------------------------------------
 ## Sample Variance versus Theoretical Variance
+## -------------------------------------------
 
+# Calculate variance of the sample distribution
+sampleDistVariance <- var(sampleDist)
+
+# Calculate hypothetical mean for the exponential distribution
+hypoDistVariance <- (1/lambda)^2
+
+## -----------------------------------
 ## Distribution: Assumpt & Conclusions
+## -----------------------------------
 
 hist(runif(1000))
 
-#---
+# ---
 
 mns = NULL
 for (i in 1:1000) mns = c(mns, mean(runif(40)))
