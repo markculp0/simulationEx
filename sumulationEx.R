e86d57be4449238455c9566a421c582fa8d87173
Mark Culp <markculp0@gmail.com>
2017-03-28 22:08:30 -0500

    Mean and var tables; recalc variance

--- sumulationEx.R
+++ sumulationEx.R
@@ -12,10 +12,7 @@
 n <- 40
 lambda <- 0.2
 
-set.seed(0)
 
-# Generate random deviates for an exponetial distribution
-sampleDist <- rexp(n, lambda)
 
 # An exploratory plot 
 # . . .
@@ -24,39 +21,70 @@ sampleDist <- rexp(n, lambda)
 ## Simulations:  
 ## -----------
 
+set.seed(0)
 
+# Generate 1,000 random deviates for an exponential distribution
+sampleExpDist <- rexp(1000, lambda)
+
+# Generate a distribution of 1000 averages of 40 random 
+# deviates by sampling the exponential distribution
+# and calculate the variances in each sample.
+expSample = NULL
+mnsExpDist = NULL
+varExpDist = NULL
+for (i in 1 : 1000) {
+  expSample = rexp(n, lambda)
+  mnsExpDist = c(mnsExpDist, mean(expSample))
+  varExpDist = c(varExpDist, var(expSample))
+}
+
+# Create a histogram of the distribution of exponentials
+hist(sampleExpDist)
+
+# Create histogram of the distribution of exponential averages
+hist(mnsExpDist)
 
 ## -----------------------------------
 ## Sample Mean versus Theoretical Mean
 ## -----------------------------------
 
-# Calculate mean of the sample distribution
-sampleDistMean <- mean(sampleDist)
+# Calculate the mean of the sample exponential distribution
+sampleExpDistMean <- round(mean(sampleExpDist),3)
+
+# Calculate mean of the distribution of exponential averages
+mnsDistMean <- round(mean(mnsExpDist),3)
 
 # Calculate hypothetical mean for the exponential distribution
-hypoDistMean <- 1/lambda
+hypoDistMean <- round(1/lambda,3)
+
+# Combine means in a row
+rowMeans <- rbind(c(sampleExpDistMean, mnsDistMean, hypoDistMean))
+
+rowMeans
 
 ## -------------------------------------------
 ## Sample Variance versus Theoretical Variance
 ## -------------------------------------------
 
-# Calculate variance of the sample distribution
-sampleDistVariance <- var(sampleDist)
+# Calculate variance of the sample exponential distribution
+sampleExpDistVariance <- round(var(sampleExpDist),3)
+
+# Calculate variance of the distribution of exponential averages
+mnsExpDistVariance <- round(mean(varExpDist),3)
 
 # Calculate hypothetical mean for the exponential distribution
 hypoDistVariance <- (1/lambda)^2
+
+# Combine variances in a row
+rowVar <- rbind(c(sampleExpDistVariance, mnsExpDistVariance, hypoDistVariance))
+
+rowVar
 
 ## -----------------------------------
 ## Distribution: Assumpt & Conclusions
 ## -----------------------------------
 
-hist(runif(1000))
 
-# ---
-
-mns = NULL
-for (i in 1:1000) mns = c(mns, mean(runif(40)))
-hist(mns)
 
 
 
